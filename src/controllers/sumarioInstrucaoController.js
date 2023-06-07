a3286d95a9c0a02e8dedaadbb75912fc1536cd5e
alfernab <86300297+alfernabreu@users.noreply.github.com>
2023-06-07 00:01:05 -0300

    adição de função

--- src/controllers/sumarioInstrucaoController.js
+++ src/controllers/sumarioInstrucaoController.js
@@ -1,3 +1,6 @@
-exports.index = (req, res) => {
-    res.render('sumarioInstrucao');
-  };
+const Instrucao = require('../models/InstrucaoModel');
+
+exports.index = async(req, res) => {
+  const instrucao = await Instrucao.buscaInstrucao();
+  res.render('sumarioInstrucao', { instrucao });
+}
